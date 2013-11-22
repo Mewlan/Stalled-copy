fccbf8f4de09f999ae9683672a52468f78569cee
Mewlan <m4wlan@icloud.com>
2013-11-22 14:33:10 +0600

    Initial Commit

---
+++ Stalled/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Stalled
+//
+//  Created by Mewlan Musajan on 11/22/13.
+//  Copyright (c) 2013 Mewlan Musajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stalled/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Stalled
+//
+//  Created by Mewlan Musajan on 11/22/13.
+//  Copyright (c) 2013 Mewlan Musajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
